fd8b65659fc728fb7d5badf4b42b9b8a09482b30
Mat191126 <matprz11@gmail.com>
2021-01-12 12:32:23 +0100

    Add condition that user must give arguments while running app

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -2,6 +2,10 @@ package com.codecool;
 
 public class ConverterApplication {
     public static void main(String[] args) {
-
+        if (args.length > 0) {
+            //TODO
+        } else {
+            System.out.println("No input file defined");
+        }
     }
 }
