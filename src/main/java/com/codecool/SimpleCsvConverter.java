c897f01868b16b68f877f1f52c570bb5b2adeff3
Mat191126 <matprz11@gmail.com>
2021-01-12 17:18:17 +0100

    add FileReader instance

--- src/main/java/com/codecool/SimpleCsvConverter.java
+++ src/main/java/com/codecool/SimpleCsvConverter.java
@@ -1,10 +1,25 @@
 package com.codecool;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class SimpleCsvConverter {
+    FileReader fileReader;
 
-    public void convert(Path filePath, FileType outputType) {
+    public SimpleCsvConverter(){
+
+    }
+
+    public SimpleCsvConverter(FileReader fileReader) {
+        this.fileReader = fileReader;
+    }
+
+    public void setFileReader(FileReader fileReader) {
+        this.fileReader = fileReader;
+    }
+
+    public void convert(Path filePath, FileType outputType) throws IOException {
         System.out.println("I convert CSV to output format " + outputType.toString().toLowerCase());
+        fileReader.readData(filePath);
     }
 }
