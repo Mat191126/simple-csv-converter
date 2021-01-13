bfb099fe342a7c39919d40c7120e347b831024b8
Mat191126 <matprz11@gmail.com>
2021-01-13 15:46:51 +0100

    Add creating jar with dependencies in pom, AppConfig refactor

--- src/main/java/com/codecool/AppConfig.java
+++ src/main/java/com/codecool/AppConfig.java
@@ -1,9 +1,20 @@
 package com.codecool;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = "com.codecool")
 public class AppConfig {
+
+    @Bean(name = "fileReader")
+    public FileReader reader() {
+        return new FileReader();
+    }
+
+    @Bean(name = "simpleCsvConverter")
+    public SimpleCsvConverter csvConverter() {
+        return new SimpleCsvConverter();
+    }
 }
