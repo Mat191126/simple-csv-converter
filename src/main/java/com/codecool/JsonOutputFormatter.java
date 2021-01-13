792b950170cc364ad1d5f72aa08a399c9d364d54
Mat191126 <matprz11@gmail.com>
2021-01-13 10:12:10 +0100

    add formatting to json file

--- src/main/java/com/codecool/JsonOutputFormatter.java
+++ src/main/java/com/codecool/JsonOutputFormatter.java
@@ -1,10 +1,52 @@
 package com.codecool;
 
+import com.jakewharton.fliptables.FlipTable;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonOutputFormatter implements OutputFormatter{
     @Override
     public void printToConsole(BufferedReader data) {
-        System.out.println("JSON");
+        int HEADER_INDEX = 0;
+        int indention = 5;
+        List<String[]> rows = new ArrayList<>();
+        String line = "";
+        while (true) {
+            try {
+                if ((line = data.readLine()) == null) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String[] dataLine = line.split(",");
+            rows.add(dataLine);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[\n");
+        for (int i = 0; i < rows.size(); i++) {
+            stringBuilder.append(" ".repeat(indention))
+                    .append("{\n");
+            for (int j = 0; j < rows.get(HEADER_INDEX).length; j++) {
+                stringBuilder.append(" ".repeat(indention*2))
+                        .append(rows.get(HEADER_INDEX)[j])
+                        .append(" : ")
+                        .append(rows.get(i)[j]);
+                if (j != rows.get(HEADER_INDEX).length - 1) {
+                    stringBuilder.append(",");
+                }
+                stringBuilder.append("\n");
+            }
+            stringBuilder.append(" ".repeat(indention))
+                    .append("}");
+            if (i != rows.size() - 1) {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append("\n");
+        }
+        stringBuilder.append("]");
+        System.out.println(stringBuilder.toString());
     }
 }
